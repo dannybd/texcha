5f33c6f2dada718f71593dd8a2675dbbd0ff461b
Daniel M Ben-David <dannybd@mit.edu>
2013-12-08 07:49:36 -0500

    First commit. Adding a README and some methods to pull md5 hashes of compiled LaTeX equations and compare them

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+function get_latex_url($latex) {
+  $base_url = 'http://latex.codecogs.com/gif.latex?';
+  return $base_url . rawurlencode($latex);
+}
+
+function get_latex_hash($latex) {
+  return md5(file_get_contents(get_latex_url($latex)));
+}
+
+function check_latex($true_hash, $latex) {
+  $test_hash = get_latex_hash($latex);
+  return $true_hash == $test_hash;
+}
+
+$sample_latex = '2 \equiv \sum_{i=0}^{\infty} \frac{1}{2^i}';
+$sample_md5 = get_latex_hash($sample_latex);
+
+$test_latex = '2\equiv\sum_{i = 0}^{{}\infty}\frac{1}{2^i}{}';
+$test_md5 = get_latex_hash($test_latex);
+
+header('Content-type: text/javascript');
+echo $sample_md5 . "\n\n" . $test_md5;
+?>
